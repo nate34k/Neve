3baedab7cc86fabfc8e1f3cce30657f80d15a16e
Nathan Kraft <nate34k@gmail.com>
2024-07-10 14:39:19 -0500

    add gruvbox theme; disable catppuccin

--- config/default.nix
+++ config/default.nix
@@ -9,6 +9,7 @@
     ./colorschemes/base16.nix
     ./colorschemes/catppuccin.nix
     ./colorschemes/rose-pine.nix
+    ./colorschemes/gruvbox.nix
 
     ./completion/cmp.nix
     ./completion/copilot.nix
